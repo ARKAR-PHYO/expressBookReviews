b4f2da60b1ef7094dfb905a5630eb38eb9f09356
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2024-07-22 16:56:07 -0400

    Task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,11 +40,24 @@ public_users.get("/", function (req, res) {
 
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    if(!req.params.isbn) return res.status(404).json({message: "Book not found with this isbn."})
-    const isbn = req.params.isbn
-    res.send(books[isbn])
- });
+// public_users.get('/isbn/:isbn',function (req, res) {
+//     if(!req.params.isbn) return res.status(404).json({message: "Book not found with this isbn."})
+//     const isbn = req.params.isbn
+//     res.send(books[isbn])
+//  });
+public_users.get("/isbn/:isbn", function (req, res) {
+  if (!req.params.isbn)
+    return res.status(404).json({ message: "Book not found with this isbn." });
+  const isbn = req.params.isbn;
+
+  let getBookByISBN = new Promise((resolve, reject) => {
+    resolve(JSON.stringify(books[isbn], null, 4));
+  });
+  getBookByISBN.then((successMessage) => {
+    console.log("From Callback " + successMessage);
+  });
+});
+
   
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
