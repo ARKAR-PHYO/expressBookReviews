508c4980a127483289b82ea0d995b91e08d798f6
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2024-07-22 15:58:21 -0400

    Get book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,9 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if(!req.params.isbn) return res.status(404).json({message: "Book not found with this isbn."})
+    const isbn = req.params.isbn
+    res.send(books[isbn])
  });
   
 // Get book details based on author
