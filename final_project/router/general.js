b552fb764fda6189b29a3f5c7c22a980eb9150c2
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2024-07-22 16:00:03 -0400

    Get book details based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,9 +23,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author;
+  if (!author) return res.status(404).json({ message: "No author provided." });
+
+  const booksByAuthor = Object.keys(books)
+    .map((key) => books[key])
+    .filter((book) => book.author === author);
+
+  if (booksByAuthor.length === 0) {
+    return res.status(404).json({ message: "No books found for this author." });
+  }
+
+  res.json(booksByAuthor);
 });
 
 // Get all books based on title
